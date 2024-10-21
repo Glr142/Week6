71453e86801e31aca6f399b3c1dd3fe2bfa46f8a
glr142 <ruddengeorgia@gmail.com>
2024-10-21 10:59:35 +0100

    First commit - Start up files

---
+++ GProject/ContactDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace GProject
+{
+    public class ContactDataService
+    {
+        public List<Person> GetContacts()
+        {
+            return new List<Person>
+            {
+                new Person("Alice", "Smith"),
+                new Person("Bob", "Johnson"),
+                new Person("Charlie", "Brown"),
+            };
+        }
+    }
+}
---
+++ GProject/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GProject
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
+    }
+}
